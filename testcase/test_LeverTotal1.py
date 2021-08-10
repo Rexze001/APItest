bff21607b2e4c737c2d8d137600c01762f137c7c
Rex001 <ximenchuiyu@foxmail.com>
2021-08-10 14:39:12 +0800

    Add data

--- testcase/test_LeverTotal1.py
+++ testcase/test_LeverTotal1.py
@@ -9,6 +9,9 @@ class TestLever:
     @pytest.mark.parametrize('classA,classB,lever', [
         [80, 160, 'Invalid result!'],
         [50, 150, 'Lever A'],
+        [50, -15, 'Wrong number!'],
+        [75, 25, 'Lever B'],
+        [0, 0, 'Lever C']
     ])
     def test_leverToal(self, classA, classB, lever):
         url = self.api_Url + "?classA={}&classB={}".format(classA, classB)
